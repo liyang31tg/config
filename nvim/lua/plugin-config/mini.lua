336c9486e8a7db1036f55189c9c3b6bd3308aca5
mdadc <mdacc@acc.cc>
2023-11-22 09:59:56 +0700

    ff

--- nvim/lua/plugin-config/mini.lua
+++ nvim/lua/plugin-config/mini.lua
@@ -1,109 +1,111 @@
 local status, minisurround = pcall(require, "mini.surround")
 if not status then
-    vim.notify("没有找到 mini.surround")
-    return
+	vim.notify("没有找到 mini.surround")
+	return
 end
 
 minisurround.setup({
-  mappings = {
-    add = 'gsa', -- Add surrounding in Normal and Visual modes
-    delete = 'gsd', -- Delete surrounding
-    replace = 'gsr', -- Replace surrounding
-    find = 'gsf', -- Find surrounding (to the right)
-    find_left = 'gsF', -- Find surrounding (to the left)
-    highlight = 'gsh', -- Highlight surrounding
-    update_n_lines = 'gsn', -- Update `n_lines`
-    suffix_last = '', -- Suffix to search with "prev" method
-    suffix_next = '', -- Suffix to search with "next" method
-  },
+	mappings = {
+		add = "gsa", -- Add surrounding in Normal and Visual modes
+		delete = "gsd", -- Delete surrounding
+		replace = "gsr", -- Replace surrounding
+		find = "gsf", -- Find surrounding (to the right)
+		find_left = "gsF", -- Find surrounding (to the left)
+		highlight = "gsh", -- Highlight surrounding
+		update_n_lines = "gsn", -- Update `n_lines`
+		suffix_last = "", -- Suffix to search with "prev" method
+		suffix_next = "", -- Suffix to search with "next" method
+	},
 })
 
 local status1, minicomment = pcall(require, "mini.comment")
 if not status1 then
-    vim.notify("没有找到 mini.comment")
-    return
+	vim.notify("没有找到 mini.comment")
+	return
 end
 
 minicomment.setup({
-  mappings = {
-    -- Toggle comment (like `gcip` - comment inner paragraph) for both
-    -- Normal and Visual modes
-    comment = ',c',
-    -- Toggle comment on current line
-    comment_line = ',cc',
-    -- Define 'comment' textobject (like `dgc` - delete whole comment block)
-    textobject = ',c',
-  },
+	mappings = {
+		-- Toggle comment (like `gcip` - comment inner paragraph) for both
+		-- Normal and Visual modes
+		comment = ",c",
+		-- Toggle comment on current line
+		comment_line = ",cc",
+		-- Toggle comment on visual selection
+		comment_visual = ",c",
+		-- Define 'comment' textobject (like `dgc` - delete whole comment block)
+		textobject = ",c",
+	},
 })
 
 local status2, minipairs = pcall(require, "mini.pairs")
 if not status2 then
-    vim.notify("没有找到 mini.pairs")
-    return
+	vim.notify("没有找到 mini.pairs")
+	return
 end
 
 minipairs.setup({
-  -- In which modes mappings from this `config` should be created
-  modes = { insert = true, command = false, terminal = false },
+	-- In which modes mappings from this `config` should be created
+	modes = { insert = true, command = false, terminal = false },
 
-  -- Global mappings. Each right hand side should be a pair information, a
-  -- table with at least these fields (see more in |MiniPairs.map|):
-  -- - <action> - one of 'open', 'close', 'closeopen'.
-  -- - <pair> - two character string for pair to be used.
-  -- By default pair is not inserted after `\`, quotes are not recognized by
-  -- `<CR>`, `'` does not insert pair after a letter.
-  -- Only parts of tables can be tweaked (others will use these defaults).
-  mappings = {
-    ['('] = { action = 'open', pair = '()', neigh_pattern = '[^\\].' },
-    ['['] = { action = 'open', pair = '[]', neigh_pattern = '[^\\].' },
-    ['{'] = { action = 'open', pair = '{}', neigh_pattern = '[^\\].' },
+	-- Global mappings. Each right hand side should be a pair information, a
+	-- table with at least these fields (see more in |MiniPairs.map|):
+	-- - <action> - one of 'open', 'close', 'closeopen'.
+	-- - <pair> - two character string for pair to be used.
+	-- By default pair is not inserted after `\`, quotes are not recognized by
+	-- `<CR>`, `'` does not insert pair after a letter.
+	-- Only parts of tables can be tweaked (others will use these defaults).
+	mappings = {
+		["("] = { action = "open", pair = "()", neigh_pattern = "[^\\]." },
+		["["] = { action = "open", pair = "[]", neigh_pattern = "[^\\]." },
+		["{"] = { action = "open", pair = "{}", neigh_pattern = "[^\\]." },
 
-    [')'] = { action = 'close', pair = '()', neigh_pattern = '[^\\].' },
-    [']'] = { action = 'close', pair = '[]', neigh_pattern = '[^\\].' },
-    ['}'] = { action = 'close', pair = '{}', neigh_pattern = '[^\\].' },
+		[")"] = { action = "close", pair = "()", neigh_pattern = "[^\\]." },
+		["]"] = { action = "close", pair = "[]", neigh_pattern = "[^\\]." },
+		["}"] = { action = "close", pair = "{}", neigh_pattern = "[^\\]." },
 
-    ['"'] = { action = 'closeopen', pair = '""', neigh_pattern = '[^\\].', register = { cr = false } },
-    ["'"] = { action = 'closeopen', pair = "''", neigh_pattern = '[^%a\\].', register = { cr = false } },
-    ['`'] = { action = 'closeopen', pair = '``', neigh_pattern = '[^\\].', register = { cr = false } },
-  },
+		['"'] = { action = "closeopen", pair = '""', neigh_pattern = "[^\\].", register = { cr = false } },
+		["'"] = { action = "closeopen", pair = "''", neigh_pattern = "[^%a\\].", register = { cr = false } },
+		["`"] = { action = "closeopen", pair = "``", neigh_pattern = "[^\\].", register = { cr = false } },
+	},
 })
 
 local status3, miniai = pcall(require, "mini.ai")
 if not status3 then
-    vim.notify("没有找到 mini.ai")
-    return
+	vim.notify("没有找到 mini.ai")
+	return
 end
 
 miniai.setup({
-   -- Table with textobject id as fields, textobject specification as values.
-  -- Also use this to disable builtin textobjects. See |MiniAi.config|.
-  custom_textobjects = nil,
+	-- Table with textobject id as fields, textobject specification as values.
+	-- Also use this to disable builtin textobjects. See |MiniAi.config|.
+	custom_textobjects = nil,
 
-  -- Module mappings. Use `''` (empty string) to disable one.
-  mappings = {
-    -- Main textobject prefixes
-    around = 'a',
-    inside = 'i',
+	-- Module mappings. Use `''` (empty string) to disable one.
+	mappings = {
+		-- Main textobject prefixes
+		around = "a",
+		inside = "i",
 
-    -- Next/last variants
-    around_next = 'an',
-    inside_next = 'in',
-    around_last = 'al',
-    inside_last = 'il',
+		-- Next/last variants
+		around_next = "an",
+		inside_next = "in",
+		around_last = "al",
+		inside_last = "il",
 
-    -- Move cursor to corresponding edge of `a` textobject
-    goto_left = 'g[',
-    goto_right = 'g]',
-  },
+		-- Move cursor to corresponding edge of `a` textobject
+		goto_left = "g[",
+		goto_right = "g]",
+	},
 
-  -- Number of lines within which textobject is searched
-  n_lines = 50,
+	-- Number of lines within which textobject is searched
+	n_lines = 50,
 
-  -- How to search for object (first inside current line, then inside
-  -- neighborhood). One of 'cover', 'cover_or_next', 'cover_or_prev',
-  -- 'cover_or_nearest', 'next', 'previous', 'nearest'.
-  search_method = 'cover_or_next',
+	-- How to search for object (first inside current line, then inside
+	-- neighborhood). One of 'cover', 'cover_or_next', 'cover_or_prev',
+	-- 'cover_or_nearest', 'next', 'previous', 'nearest'.
+	search_method = "cover_or_next",
 
-  -- Whether to disable showing non-error feedback
-  silent = false,
+	-- Whether to disable showing non-error feedback
+	silent = false,
 })
