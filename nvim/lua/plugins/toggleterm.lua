585bcdecb7c01c71a67c3316707d7c054e94a223
jjkkww <cchiasfj@164.com>
2024-03-19 20:12:35 +0700

    fix term

--- nvim/lua/plugins/toggleterm.lua
+++ nvim/lua/plugins/toggleterm.lua
@@ -1,85 +1,86 @@
 local opt = {
-    size = 20,
-    -- TODO: add my own keymapping to <space-t>
-    open_mapping = [[<c-\>]],
-    hide_numbers = false,
-    shade_filetypes = {},
-    shade_terminals = true,
-    shading_factor = 3,
-    start_in_insert = true,
-    insert_mappings = true,
-    persist_size = true,
-    direction = "float",
-    close_on_exit = true,
-    shell = vim.o.shell,
-    float_opts = {
-        border = "curved",
-        winblend = 3,
-        highlights = {
-            border = "Normal",
-            background = "Normal",
-        },
-    },
+	size = 20,
+	-- TODO: add my own keymapping to <space-t>
+	open_mapping = [[<c-\>]],
+	hide_numbers = false,
+	shade_filetypes = {},
+	shade_terminals = true,
+	shading_factor = 3,
+	start_in_insert = true,
+	insert_mappings = true,
+	persist_size = true,
+	direction = "float",
+	close_on_exit = true,
+	shell = vim.o.shell,
+	float_opts = {
+		border = "curved",
+		winblend = 3,
+		highlights = {
+			border = "Normal",
+			background = "Normal",
+		},
+	},
 }
 
 local obj = {
-    "akinsho/toggleterm.nvim",
-    version = "*",
-    opts = opt,
-    config = function(_, opts)
-        require("toggleterm").setup(opts)
-        function _G.set_terminal_keymaps()
-            local optss = { noremap = true }
-            vim.api.nvim_buf_set_keymap(0, "t", "<esc>", [[<C-\><C-n>]], optss)
-            vim.api.nvim_buf_set_keymap(0, "t", "<C-h>", [[<C-\><C-n><C-W>h]], optss)
-            vim.api.nvim_buf_set_keymap(0, "t", "<C-j>", [[<C-\><C-n><C-W>j]], optss)
-            vim.api.nvim_buf_set_keymap(0, "t", "<C-k>", [[<C-\><C-n><C-W>k]], optss)
-            -- ctrl l preserved for clear terminal content
-            -- vim.api.nvim_buf_set_keymap(0, 't', '<C-l>', [[<C-\><C-n><C-W>l]], opts)
-        end
+	"akinsho/toggleterm.nvim",
+	version = "*",
+	opts = opt,
+	config = function(_, opts)
+		require("toggleterm").setup(opts)
+		function _G.set_terminal_keymaps()
+			local optss = { noremap = true }
+			vim.api.nvim_buf_set_keymap(0, "t", "<esc>", [[<C-\><C-n>]], optss)
+			-- vim.api.nvim_buf_set_keymap(0, "t", "<C-h>", [[<C-\><C-n><C-W>h]], optss)
+			-- vim.api.nvim_buf_set_keymap(0, "t", "<C-j>", [[<C-\><C-n><C-W>j]], optss)
+			-- vim.api.nvim_buf_set_keymap(0, "t", "<C-k>", [[<C-\><C-n><C-W>k]], optss)
+			vim.api.nvim_buf_set_keymap(0, "t", "<C-t>", [[<C-\><C-n><cmd>ToggleTerm<cr>]], optss)
+			-- ctrl l preserved for clear terminal content
+			-- vim.api.nvim_buf_set_keymap(0, 't', '<C-l>', [[<C-\><C-n><C-W>l]], opts)
+		end
 
-        vim.cmd("autocmd! TermOpen term://* lua set_terminal_keymaps()")
+		vim.cmd("autocmd! TermOpen term://* lua set_terminal_keymaps()")
 
-        local Terminal = require("toggleterm.terminal").Terminal
-        local lazygit = Terminal:new({ cmd = "lazygit", hidden = true })
+		local Terminal = require("toggleterm.terminal").Terminal
+		local lazygit = Terminal:new({ cmd = "lazygit", hidden = true })
 
-        -- sudo add-apt-repository ppa:lazygit-team/release
-        -- sudo apt-get update
-        -- sudo apt-get install lazygit
-        function _LAZYGIT_TOGGLE()
-            lazygit:toggle()
-        end
+		-- sudo add-apt-repository ppa:lazygit-team/release
+		-- sudo apt-get update
+		-- sudo apt-get install lazygit
+		function _LAZYGIT_TOGGLE()
+			lazygit:toggle()
+		end
 
-        local gitui = Terminal:new({ cmd = "gitui", hidden = true })
+		local gitui = Terminal:new({ cmd = "gitui", hidden = true })
 
-        function _GITUI_TOGGLE()
-            gitui:toggle()
-        end
+		function _GITUI_TOGGLE()
+			gitui:toggle()
+		end
 
-        local node = Terminal:new({ cmd = "node", hidden = true })
+		local node = Terminal:new({ cmd = "node", hidden = true })
 
-        function _NODE_TOGGLE()
-            node:toggle()
-        end
+		function _NODE_TOGGLE()
+			node:toggle()
+		end
 
-        local ncdu = Terminal:new({ cmd = "ncdu", hidden = true })
+		local ncdu = Terminal:new({ cmd = "ncdu", hidden = true })
 
-        function _NCDU_TOGGLE()
-            ncdu:toggle()
-        end
+		function _NCDU_TOGGLE()
+			ncdu:toggle()
+		end
 
-        local htop = Terminal:new({ cmd = "htop", hidden = true })
+		local htop = Terminal:new({ cmd = "htop", hidden = true })
 
-        function _HTOP_TOGGLE()
-            htop:toggle()
-        end
+		function _HTOP_TOGGLE()
+			htop:toggle()
+		end
 
-        local python = Terminal:new({ cmd = "python", hidden = true })
+		local python = Terminal:new({ cmd = "python", hidden = true })
 
-        function _PYTHON_TOGGLE()
-            python:toggle()
-        end
-    end,
+		function _PYTHON_TOGGLE()
+			python:toggle()
+		end
+	end,
 }
 
 return obj
