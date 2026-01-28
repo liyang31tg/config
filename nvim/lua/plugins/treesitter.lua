73b90ebd6818e563a245b8cf8981449dbff68023
cc <dd@cc.com>
2026-01-28 20:45:11 +0700

    fix

--- nvim/lua/plugins/treesitter.lua
+++ nvim/lua/plugins/treesitter.lua
@@ -1,75 +1,75 @@
 vim.opt.runtimepath:append("$HOME/.local/share/treesitter") --mark treesitter 存放目录,且下面必须指定目录位置,否则无法找到,将每次打开都重新安装一次
 local opts = {
-    -- 安装 language parser
-    -- :TSInstallInfo 命令查看支持的语言
-    parser_install_dir = "$HOME/.local/share/treesitter",
-    auto_install = true, --进入buffer,检测到没有就安装
-    sync_install = false,
-    ensure_installed = { "json", "html", "css", "vim", "lua", "javascript", "typescript", "go" },
-    -- ensure_installed = "all", 启用代码高亮模块
-    highlight = {
-        enable = true,
-        disable = { "lua" },                 -- 不要加强lua高亮,因为keybind那个文件会卡出翔
-        additional_vim_regex_highlighting = false, --关闭vim的正则高亮
-    },
-    indent = {
-        enable = true,
-    },
-    textobjects = {
-        select = {
-            enable = true,
+	-- 安装 language parser
+	-- :TSInstallInfo 命令查看支持的语言
+	parser_install_dir = "$HOME/.local/share/treesitter",
+	auto_install = true, --进入buffer,检测到没有就安装
+	sync_install = false,
+	ensure_installed = { "json", "html", "css", "vim", "lua", "javascript", "typescript", "go" },
+	-- ensure_installed = "all", 启用代码高亮模块
+	highlight = {
+		enable = true,
+		disable = { "lua" }, -- 不要加强lua高亮,因为keybind那个文件会卡出翔
+		additional_vim_regex_highlighting = false, --关闭vim的正则高亮
+	},
+	indent = {
+		enable = true,
+	},
+	textobjects = {
+		select = {
+			enable = true,
 
-            -- Automatically jump forward to textobj, similar to targets.vim
-            lookahead = true,
+			-- Automatically jump forward to textobj, similar to targets.vim
+			lookahead = true,
 
-            keymaps = {
-                -- You can use the capture groups defined in textobjects.scm
-                ["af"] = "@function.outer",
-                ["if"] = "@function.inner",
-                ["ac"] = "@class.outer",
-                -- You can optionally set descriptions to the mappings (used in the desc parameter of
-                -- nvim_buf_set_keymap) which plugins like which-key display
-                ["ic"] = { query = "@class.inner", desc = "Select inner part of a class region" },
-                -- You can also use captures from other query groups like `locals.scm`
-                ["as"] = { query = "@scope", query_group = "locals", desc = "Select language scope" },
-            },
-            -- You can choose the select mode (default is charwise 'v')
-            --
-            -- Can also be a function which gets passed a table with the keys
-            -- * query_string: eg '@function.inner'
-            -- * method: eg 'v' or 'o'
-            -- and should return the mode ('v', 'V', or '<c-v>') or a table
-            -- mapping query_strings to modes.
-            selection_modes = {
-                ["@parameter.outer"] = "v", -- charwise
-                ["@function.outer"] = "V", -- linewise
-                ["@class.outer"] = "<c-v>", -- blockwise
-            },
-            -- If you set this to `true` (default is `false`) then any textobject is
-            -- extended to include preceding or succeeding whitespace. Succeeding
-            -- whitespace has priority in order to act similarly to eg the built-in
-            -- `ap`.
-            --
-            -- Can also be a function which gets passed a table with the keys
-            -- * query_string: eg '@function.inner'
-            -- * selection_mode: eg 'v'
-            -- and should return true of false
-            include_surrounding_whitespace = false,
-        },
-    },
+			keymaps = {
+				-- You can use the capture groups defined in textobjects.scm
+				["af"] = "@function.outer",
+				["if"] = "@function.inner",
+				["ac"] = "@class.outer",
+				-- You can optionally set descriptions to the mappings (used in the desc parameter of
+				-- nvim_buf_set_keymap) which plugins like which-key display
+				["ic"] = { query = "@class.inner", desc = "Select inner part of a class region" },
+				-- You can also use captures from other query groups like `locals.scm`
+				["as"] = { query = "@scope", query_group = "locals", desc = "Select language scope" },
+			},
+			-- You can choose the select mode (default is charwise 'v')
+			--
+			-- Can also be a function which gets passed a table with the keys
+			-- * query_string: eg '@function.inner'
+			-- * method: eg 'v' or 'o'
+			-- and should return the mode ('v', 'V', or '<c-v>') or a table
+			-- mapping query_strings to modes.
+			selection_modes = {
+				["@parameter.outer"] = "v", -- charwise
+				["@function.outer"] = "V", -- linewise
+				["@class.outer"] = "<c-v>", -- blockwise
+			},
+			-- If you set this to `true` (default is `false`) then any textobject is
+			-- extended to include preceding or succeeding whitespace. Succeeding
+			-- whitespace has priority in order to act similarly to eg the built-in
+			-- `ap`.
+			--
+			-- Can also be a function which gets passed a table with the keys
+			-- * query_string: eg '@function.inner'
+			-- * selection_mode: eg 'v'
+			-- and should return true of false
+			include_surrounding_whitespace = false,
+		},
+	},
 }
 
 local obj = {
-    "nvim-treesitter/nvim-treesitter",
-    main = "nvim-treesitter.configs",
-    event = "VeryLazy",
-    dependencies = {
-        "nvim-treesitter/nvim-treesitter-textobjects",
-    },
-    build = ":TSUpdate",
-    config = function()
-        require("nvim-treesitter.configs").setup(opts)
-    end,
+	"nvim-treesitter/nvim-treesitter",
+	branch = "master",
+	lazy = false,
+	build = ":TSUpdate",
+	dependencies = {
+		"nvim-treesitter/nvim-treesitter-textobjects",
+	},
+	config = function()
+		require("nvim-treesitter.configs").setup(opts)
+	end,
 }
 
 return obj
