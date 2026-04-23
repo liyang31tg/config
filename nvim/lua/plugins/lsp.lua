6066930b38d171bd7f895bed0644ad82b6f6d142
cc <cc@1t.com>
2026-04-23 13:00:07 +0700

    fix

--- nvim/lua/plugins/lsp.lua
+++ nvim/lua/plugins/lsp.lua
@@ -1,14 +1,8 @@
-function GenServers()
-	local vue_language_server_path = vim.fn.expand("$MASON/packages")
+local function GenServers()
+	-- 路径（字符串，不要包装table！）
+	local vue_plugin_path = vim.fn.expand("$MASON/packages")
 		.. "/vue-language-server"
 		.. "/node_modules/@vue/language-server"
-	-- local vue_language_server_path = '/path/to/@vue/language-server'
-	local vue_plugin = {
-		name = "@vue/typescript-plugin",
-		location = vue_language_server_path,
-		languages = { "vue" },
-		configNamespace = "typescript",
-	}
 
 	local servers = {
 		lua_ls = {
@@ -16,97 +10,28 @@ function GenServers()
 				Lua = {
 					workspace = { checkThirdParty = false },
 					telemetry = { enable = false },
-					diagnostics = {
-						globals = { "vim" },
-					},
+					diagnostics = { globals = { "vim" } },
 				},
 			},
 		},
+
 		gopls = {
 			settings = {
 				gopls = {
 					completeUnimported = true,
 					usePlaceholders = true,
-					analyses = {
-						unusedparams = true,
-					},
+					analyses = { unusedparams = true },
 				},
 			},
-			cmd = { "gopls" },
-			-- 1. 修正文件类型
 			filetypes = { "go", "gomod", "gowork", "gotmpl" },
 		},
+
 		vimls = {},
 		cssls = {},
 		html = {},
 		bashls = {},
 		jsonls = {},
-		-- tsserver = {},
-		ts_ls = {
-			single_file_support = false,
-			-- https://lsp-zero.netlify.app/blog/configure-volar-v2.html  参考文章
-			init_options = {
-				plugins = {
-					{
-						name = "@vue/typescript-plugin",
-						-- TODO:
-						location = vue_language_server_path,
-						languages = { "vue" },
-					},
-				},
-			},
-			filetypes = {
-				"javascript",
-				"javascriptreact",
-				"javascript.jsx",
-				"typescript",
-				"typescriptreact",
-				"typescript.tsx",
-				"vue",
-			},
-		},
-		--好像下面2个不作用于vue
-		vtsls = {
-			init_options = {
-				plugins = {
-					vue_plugin,
-				},
-			},
-			filetypes = { "typescript", "javascript", "javascriptreact", "typescriptreact", "vue" },
-		},
-		vue_ls = {
-			on_init = function(client)
-				client.handlers["tsserver/request"] = function(_, result, context)
-					local clients = vim.lsp.get_clients({ bufnr = context.bufnr, name = "vtsls" })
-					if #clients == 0 then
-						vim.notify(
-							"Could not found `vtsls` lsp client, vue_lsp would not work with it.",
-							vim.log.levels.ERROR
-						)
-						return
-					end
-					local ts_client = clients[1]
-
-					local param = unpack(result)
-					local id, command, payload = unpack(param)
-					ts_client:exec_cmd({
-						command = "typescript.tsserverRequest",
-						arguments = {
-							command,
-							payload,
-						},
-					}, { bufnr = context.bufnr }, function(_, r)
-						local response_data = { { id, r.body } }
-						---@diagnostic disable-next-line: param-type-mismatch
-						client:notify("tsserver/response", response_data)
-					end)
-				end
-			end,
-		},
-		-- volar = {}, --masoninstall vue-language-server@1.8.27, up this version not work in vue3 . 这个错误是因为2.0.0以后得版本不再内嵌ts
 		yamlls = {},
-		-- "vuels"
-		-- denols = {}, --这个不能安装，会强制import以./ ../ 等开头的问
 		dockerls = {},
 		docker_compose_language_service = {},
 		basedpyright = {},
@@ -116,49 +41,64 @@ function GenServers()
 				"clangd",
 				"--header-insertion=never",
 				"--query-driver=/opt/homebrew/opt/llvm/bin/clang",
-				"--all-scopes-completion",
-				"--completion-style=detailed",
 			},
-			filetypes = { "c", "cpp", "objc", "objcpp", "cuda", "proto" },
-			root_dir = require("lspconfig").util.root_pattern(
-				"CMakeLists.txt",
-				".clangd",
-				".clang-tidy",
-				".clang-format",
-				"compile_commands.json",
-				"compile_flags.txt",
-				"configure.ac",
-				".git"
-			),
-			single_file_support = true,
+			filetypes = { "c", "cpp", "objc", "objcpp" },
+		},
+
+		-- ===========================================
+		-- ✅ Vue3 官方唯一支持：ts_ls （不是 vtsls！）
+		-- ===========================================
+		ts_ls = {
+			filetypes = {
+				"javascript",
+				"typescript",
+				"vue",
+				"javascriptreact",
+				"typescriptreact",
+			},
+			init_options = {
+				plugins = {
+					{
+						name = "@vue/typescript-plugin",
+						location = vue_plugin_path,
+						languages = { "vue" },
+					},
+				},
+			},
+		},
+
+		-- ===========================================
+		-- ✅ Vue 3 官方 LSP (Volar 2.x)
+		-- ===========================================
+		vue_ls = {
+			filetypes = { "vue" },
 		},
 	}
+
 	return servers
 end
 
 local obj = {
 	"neovim/nvim-lspconfig",
 	dependencies = {
-		"hrsh7th/cmp-nvim-lsp",
 		"williamboman/mason.nvim",
+		"hrsh7th/cmp-nvim-lsp",
 		"folke/neodev.nvim",
 		"ray-x/lsp_signature.nvim",
-	},
-	opts = {
-		document_highlight = { enabled = false },
 	},
 	config = function()
 		require("neodev").setup()
 		require("lsp_signature").setup()
 		require("mason").setup()
-		-- 3. 引入 lspconfig 插件
-		-- local lspconfig = require("lspconfig")
-		local servers = GenServers()
-		for server, config in pairs(servers) do
-			-- lspconfig[server].setup(config)
-			vim.lsp.config(server, config) --原生的需要手动注入一些配置,而使用上面这种插件注入,会有一些默认配置省略了很多配置
-			vim.lsp.enable(server)
-		end
+
+		-- 延迟启动，保证Mason就绪
+		vim.schedule(function()
+			local servers = GenServers()
+			for s, c in pairs(servers) do
+				vim.lsp.config(s, c)
+				vim.lsp.enable(s)
+			end
+		end)
 	end,
 }
 
--- nvim/lua/plugins/treesitter.lua
+++ nvim/lua/plugins/treesitter.lua
@@ -1,64 +1,3 @@
--- vim.opt.runtimepath:append("$HOME/.local/share/treesitter") --mark treesitter 存放目录,且下面必须指定目录位置,否则无法找到,将每次打开都重新安装一次
--- local opts = {
--- 	-- 安装 language parser
--- 	-- :TSInstallInfo 命令查看支持的语言
--- 	parser_install_dir = "$HOME/.local/share/treesitter",
--- 	auto_install = true, --进入buffer,检测到没有就安装
--- 	sync_install = false,
--- 	ensure_installed = { "json", "html", "css", "vim", "lua", "javascript", "typescript", "go" },
--- 	-- ensure_installed = "all", 启用代码高亮模块
--- 	highlight = {
--- 		enable = true,
--- 		disable = { "lua" }, -- 不要加强lua高亮,因为keybind那个文件会卡出翔
--- 		additional_vim_regex_highlighting = false, --关闭vim的正则高亮
--- 	},
--- 	indent = {
--- 		enable = true,
--- 	},
--- 	textobjects = {
--- 		select = {
--- 			enable = true,
---
--- 			-- Automatically jump forward to textobj, similar to targets.vim
--- 			lookahead = true,
---
--- 			keymaps = {
--- 				-- You can use the capture groups defined in textobjects.scm
--- 				["af"] = "@function.outer",
--- 				["if"] = "@function.inner",
--- 				["ac"] = "@class.outer",
--- 				-- You can optionally set descriptions to the mappings (used in the desc parameter of
--- 				-- nvim_buf_set_keymap) which plugins like which-key display
--- 				["ic"] = { query = "@class.inner", desc = "Select inner part of a class region" },
--- 				-- You can also use captures from other query groups like `locals.scm`
--- 				["as"] = { query = "@scope", query_group = "locals", desc = "Select language scope" },
--- 			},
--- 			-- You can choose the select mode (default is charwise 'v')
--- 			--
--- 			-- Can also be a function which gets passed a table with the keys
--- 			-- * query_string: eg '@function.inner'
--- 			-- * method: eg 'v' or 'o'
--- 			-- and should return the mode ('v', 'V', or '<c-v>') or a table
--- 			-- mapping query_strings to modes.
--- 			selection_modes = {
--- 				["@parameter.outer"] = "v", -- charwise
--- 				["@function.outer"] = "V", -- linewise
--- 				["@class.outer"] = "<c-v>", -- blockwise
--- 			},
--- 			-- If you set this to `true` (default is `false`) then any textobject is
--- 			-- extended to include preceding or succeeding whitespace. Succeeding
--- 			-- whitespace has priority in order to act similarly to eg the built-in
--- 			-- `ap`.
--- 			--
--- 			-- Can also be a function which gets passed a table with the keys
--- 			-- * query_string: eg '@function.inner'
--- 			-- * selection_mode: eg 'v'
--- 			-- and should return true of false
--- 			include_surrounding_whitespace = false,
--- 		},
--- 	},
--- }
-
 local api = vim.api
 local ts = vim.treesitter
 
@@ -116,6 +55,18 @@ api.nvim_create_autocmd("FileType", {
 		vim.wo.foldlevel = 99
 	end,
 })
+
+-- #############################################################################
+-- 👇 【关键】手动注册 Vue 解析器（Neovim 内置没有，必须加这一段！）
+-- #############################################################################
+-- vim.treesitter.language.register("vue", "vue")
+
+-- 确保 Vue 用 HTML + TS + CSS 完整高亮
+-- vim.filetype.add({
+-- 	extension = {
+-- 		vue = "vue",
+-- 	},
+-- })
 
 local obj = {
 	"nvim-treesitter/nvim-treesitter-textobjects",
