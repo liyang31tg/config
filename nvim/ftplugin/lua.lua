044c366f402ab3ca81e311a10def2882ec076942
cc <cc@1t.com>
2026-06-05 10:24:07 +0700

    fix

--- nvim/ftplugin/lua.lua
+++ nvim/ftplugin/lua.lua
@@ -1,5 +1,6 @@
-vim.keymap.set("n", "<leader>r", ":terminal lua %<CR>", {
-	buffer = 0,
-	noremap = true,
-	silent = true,
-})
+-- vim.keymap.set("n", "<leader>r", ":terminal lua %<CR>", {
+-- 	buffer = 0,
+-- 	noremap = true,
+-- 	silent = true,
+-- })
+vim.keymap.set("n", "<leader>r", "<cmd>terminal lua %:p<CR>", { buffer = 0, noremap = true, silent = true })
